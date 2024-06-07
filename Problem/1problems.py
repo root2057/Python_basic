b985ba91100283d7de6ca1e45a3c5907e469c9ed
Suman <sumankunwar1011@gmail.com>
2024-06-07 11:23:57 +0545

    solving problems

---
+++ Problem/1problems.py
@@ -0,0 +1,53 @@
+# Write a program to display a person's name, age and addresss in three different lines
+'''  name = "samir sir"
+age = 23
+address = "Kanchan - 02 Nepal"
+
+print(name)
+print(age)
+print(address) 
+'''
+
+#write a program to swap two vairables
+#method1
+'''x=12
+y=13
+
+temp = x 
+print(temp)
+x = y
+print(x)
+y=temp 
+print("value of x is", X)
+print("value of Y is", Y)
+'''
+
+
+#write a program to convert a float into integer
+x = 12.34
+print(type(x))
+
+x = int(x)
+print(type(x))
+
+#Write a program to take details from a student for id-card and then print it in different lines
+name = input("enter the name of the student: ")
+age = input("enter the age of the student: ")
+grade = input("enter the grade of the student: ")
+email = input("enter the email of the student: ")
+ph_no = input("enter the Phone number of the student: ")
+print("Student Identity Card")
+print("Name", name)
+print("Age", age)
+print("Grade", grade)
+print("Phone Number", ph_no)
+
+
+#write a program to take an user input as integer then convert it to float
+
+number = int(input("Enter an integer number: "))
+print(number)
+
+number = float(number)
+print("after conversion", a)
+print(type(number))
