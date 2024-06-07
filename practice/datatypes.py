7671dda4268e4374383ed3e462a8a843d270267a
Suman <sumankunwar1011@gmail.com>
2024-06-07 11:26:15 +0545

    update

---
+++ practice/datatypes.py
@@ -0,0 +1,12 @@
+name = input("Enter you name : ")
+print(name)
+
+age = int(input("enter you age: "))
+print(age)
+
+length = float(input("enter the length of the rectange: "))
+print(length)
+
+
+exp1 = eval(input("enter any equation here : "))
+print(exp1)
---
+++ practice/typecasting.py
@@ -0,0 +1,23 @@
+name = "samir fullel"
+print(type(name))
+
+age = 23
+print(type(age))
+
+a = 32.3
+print(type(a))
+
+b=23
+
+c = a + b 
+print(c)
+print(type(c))
+
+
+
+# x = "234"
+# y=23
+
+# xm= int("after converstion", x)
+# z= x+ xm 
+# print(z)
