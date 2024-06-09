aa08b82e88c6375de4fdff781541078022375e38
Suman <sumankunwar1011@gmail.com>
2024-06-09 20:19:34 +0545

    module in python

---
+++ practice/modules.py
@@ -0,0 +1,16 @@
+#data and time 
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+
+import random 
+l = ["Heads", "Tails"]
+x = random.choice(l)
+print(x)
+
+
+import math
+x - max(23, 32, 3253)
+print("the maximum value is", x)
